61d3f0abff0c6f522bed8468a8e4aa28441ceff7
Alexander Semyonov <rotuka@rotuka.com>
2009-08-17 19:25:25 +0400

    Added YARD documentation for Espresso::Model

--- lib/espresso/model.rb
+++ lib/espresso/model.rb
@@ -1,6 +1,7 @@
 require 'searchlogic'
 
 module Espresso
+  # @author Alexander Semyonov
   module Model
     def self.included(model)
       model.extend ClassMethods
@@ -13,6 +14,14 @@ module Espresso
     end
 
     module ClassMethods
+      # Paginates search results
+      #
+      # @param [Integer] page number of results’ page
+      # @param [Hash] query searchlogic fields (proc’ed named scopes’ names with values)
+      # @param [String] simple_query params for simple «LIKE '%something%'» searches (e.g. /people?q=Alexander)
+      # @return [Array] searchlogic object and collection of results
+      #
+      # @todo Add an options to paginating
       def paginate_found(page = nil, query = nil, simple_query = nil)
         query ||= {}
         query.merge(parse_simple_query(simple_query)) if simple_query.present?
@@ -21,17 +30,28 @@ module Espresso
         [@search, @results]
       end
 
+      # Make searchlogic query from simple query option
+      # Needed to be reimplemented in subclasses
+      #
+      # @param [String] simple query string
+      # @return [Hash] searchlogic query
       def parse_simple_query(query)
         {:"#{name_field}_like" => query}
       end
 
+      # «NameField» is a main field, used to represent model in to_s method and in simple queries
+      # @param [Symbol, String] new_name_field new field name
+      # @return [Symbol] field name
       def name_field(new_name_field = nil)
         if new_name_field.present?
-          @@name_field = new_name_field
+          @@name_field = new_name_field.to_sym
         end
         @@name_field
       end
 
+      # Make a slug from object’s NameField
+      # @param [ActiveRecord::Base] object, which slug is making
+      # @return [String] slug
       def make_slug(object)
         object.send(name_field).parameterize
       end
