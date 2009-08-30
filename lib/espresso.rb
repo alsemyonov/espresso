69e3abbf3e8795f69f083c5cfbbcd2dec0915356
Alexander Semyonov <rotuka@rotuka.com>
2009-08-31 03:24:52 +0400

    Version bump to 0.0.4

--- lib/espresso.rb
+++ lib/espresso.rb
@@ -1,2 +1,9 @@
 module Espresso
 end
+
+require 'espresso/objects_controller'
+require 'espresso/model'
+
+if defined? Rails
+  ActiveRecord::Base.send :include, Espresso::Model if defined? ActiveRecord
+end
