c19416d1775864facffe62e7f92a85d4652e9f09
Alexander Semyonov <rotuka@rotuka.com>
2009-09-01 07:47:47 +0400

    added default l10n’s

--- lib/espresso.rb
+++ lib/espresso.rb
@@ -1,4 +1,7 @@
 module Espresso
+  def self.locale_files
+    Dir[File.join(File.dirname(__FILE__), 'sugar', 'locales', '*')]
+  end
 end
 
 require 'espresso/objects_controller'
@@ -7,4 +10,5 @@ require 'espresso/model'
 if defined? Rails
   ActiveRecord::Base.send :include, Espresso::Model if defined? ActiveRecord
   ActionView::Base.send :include, Espresso::Helpers if defined? ActionView
+  I18n.load_path.unshift(*Sugar.locale_files)
 end
